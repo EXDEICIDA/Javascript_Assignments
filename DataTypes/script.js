1984278ea049f4480379ca7ee6e9b5ad2fbfe071
Javad Soltanov <153072389+EXDEICIDA@users.noreply.github.com>
2024-07-17 21:11:32 +0300

    Data Types

---
+++ DataTypes/script.js
@@ -0,0 +1,38 @@
+let country = "Portugal";
+let continent = "Europe";
+let population = 20;
+//This is an firts assignment for the javascript.
+/*
+I know now how to code in javascript hahahah
+
+*/
+console.log(country);
+console.log(continent);
+
+console.log(population);
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+console.log(typeof true);
+
+//Question:2
+/*
+Declare a variable called isIsland and 
+set its value according to your country.
+ The variable should hold a Boolean value.
+  Also declare a variable language, but don't 
+  assign it any value yet.
+*/
+
+//let isIsland = false;
+//let language;
+
+//console.log(typeof isIsland);
+//console.log(typeof population);
+//console.log(typeof country);
+//console.log(typeof language);
+
+let isIsland = "Azerbaijan";
+let langauge;
+
+console.log(typeof isIsland);
