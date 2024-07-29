ba29f6ad43e11b850399c2216e70c4942e505ed6
Javad Soltanov <153072389+EXDEICIDA@users.noreply.github.com>
2024-07-29 20:06:32 +0300

    Add files via upload

---
+++ let country = Portugal.js
@@ -0,0 +1,65 @@
+//let country = "Portugal";
+//let continent = "Europe";
+//let population = 20;
+//This is an firts assignment for the javascript.
+/*
+I know now how to code in javascript hahahah
+
+*/
+//console.log(country);
+//console.log(continent);
+
+//console.log(population);
+
+//let javascriptIsFun = true;
+//console.log(javascriptIsFun);
+//console.log(typeof true);
+
+//Question:2
+/*
+Declare a variable called isIsland and 
+set its value according to your country.
+ The variable should hold a Boolean value.
+  Also declare a variable language, but don't 
+  assign it any value yet.
+*/
+
+//let isIsland = false;
+//let language;
+
+//console.log(typeof isIsland);
+//console.log(typeof population);
+//console.log(typeof country);
+//console.log(typeof language);
+
+//let isIsland = "Azerbaijan";
+//let langauge;
+
+//console.log(typeof isIsland);
+
+//let age = 36;
+//age = 31;
+
+//const birthYear = 2004;
+//birthYear = 30;
+
+//var job = (programmer = "Programmer");
+//job = "teacher";
+
+//const now = 2037;
+
+//const ageJonas = now - 1991;
+///const ageSarah = now - 2018;
+///console.log(ageJonas);
+
+//const firstName = "Javad";
+//const lastName = "Soltanov";
+//console.log(firstName + " " + lastName);
+
+// 5 falsy values: 0 ,"", undefined,null,Nan,false
+
+console.log(Boolan(0));
+console.log(Boolean(Undefined));
+console.log(Boolean("Jonas"));
+console.log(Boolean({}));
+console.log(Boolean(""));
