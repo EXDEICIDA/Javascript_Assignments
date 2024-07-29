699f8253ca319fed10ae84eb92848284d0642c71
Javad Soltanov <153072389+EXDEICIDA@users.noreply.github.com>
2024-07-29 20:04:23 +0300

    Add files via upload

---
+++ if-else.js
@@ -0,0 +1,16 @@
+const age = 18;
+if (age === 18) {
+  console.log("You are eligible to drink a water?");
+} else {
+  console.log("You are not eligible to drink a whiskey?>");
+}
+
+const favorite = prompt("What's your favorite number?");
+console.log(favorite);
+console.log(typeof favorite);
+
+if (favorite === 23) {
+  console.log("Cool.23 is n amazing number.");
+} else {
+  console.log("Depends");
+}
