8c9ff0624c65d7873098e0075583e1a8b11a2176
Javad Soltanov <153072389+EXDEICIDA@users.noreply.github.com>
2024-07-29 05:42:45 +0300

    Add files via upload

---
+++ stringTemplate.js
@@ -0,0 +1,10 @@
+const firstName = "Javad";
+const job = "student";
+const year = 2004;
+
+const introduction =
+  "I'm " + firstName + " and I'm a student." + "I was born in " + year;
+console.log(introduction);
+
+const introduction2 = `Im ${firstName} and i was born in ${year}`;
+console.log(introduction2);
